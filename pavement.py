41d371ef260c939083961634816260ede77de3e6
James Juett <jjuett@umich.edu>
2020-08-26 11:30:33 -0400

    turn dynamic back on

--- pavement.py
+++ pavement.py
@@ -43,7 +43,7 @@ options(
                        'login_required':'false',
                        'course_title': project_name,
                        'appname':master_app,
-                       'dynamic_pages': False,
+                       'dynamic_pages': True,
                        'loglevel': 10,
                        'course_url':master_url,
                        'use_services': 'true',
